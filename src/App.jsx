b95cf59cc6800873df4cea8d8dee741e1fa6d06c
vigneshtsv~ <vigneshts27@gmail.com>
2023-12-25 11:55:11 +0530

    add remove card function

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ function App() {
   let [cart,setCart] = useState(0)
   return <>
       <TapBar cart={cart} setCart={setCart}/>
-      {/* <Header /> */}
+      <Header />
       <section className="py-5">
         <div className="container px-4 px-lg-5 mt-5">
           <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
-            <Card />
+            
+            <Card cart={cart} setCart={setCart}/>
+            <Card cart={cart} setCart={setCart}/>
           </div>
         </div>
       </section>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 
 function Card({cart,setCart}) {
+  let [toggle,setToggle]= useState(true)
   return <div className="col mb-5">
   <div className="card h-100">
       <div className="badge bg-dark text-white position-absolute" style={{top: "0.5rem",right: "0.5rem"}}>Sale</div>
-      <img className="card-img-top" src="https://www.shutterstock.com/image-photo/stability-cushion-running-shoes-new-260nw-1958445436.jpg" alt="..." />
+      <img className="card-img-top" src="https://www.shutterstock.com/image-photo/stability-cushion-running-shoes-new-260nw-1958445436.jpg" alt="shoes" />
       <div className="card-body p-4">
           <div className="text-center">
               <h5 className="fw-bolder">Sale Item</h5>
@@ -13,8 +14,17 @@ function Card({cart,setCart}) {
           </div>
       </div>
       <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
-          <div className="text-center"><button className="btn btn-outline-dark mt-auto" onClick={()=>{setCart(cart+1);console.log(cart)}}>
-            Add to cart</button>
+          <div className="text-center">
+            {
+            toggle?<button className="btn btn-outline-dark mt-auto" onClick={()=>{
+              setCart(cart+1)
+              setToggle(false)
+            }}>Add to cart</button>:
+            <button className="btn btn-outline-dark mt-auto" onClick={()=>{
+              setCart(cart-1)
+              setToggle(true)
+            }}>Remove from Cart</button>
+          }
             </div>
       </div>
   </div>
